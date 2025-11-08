944f94d8ec451048bc18af348bc77cbb6bab5726
zainalsalamun <zainalrtf@gmail.com>
2025-11-08 22:54:33 +0700

    add : authRoutes, userRoutes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import invoiceRoutes from "./routes/invoiceRoutes.js";
 import customerRoute from "./routes/customerRoute.js"; 
+import authRoutes from "./routes/authRouter.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -14,5 +16,13 @@ app.use(express.json());
 // routes
 app.use("/api/invoices", invoiceRoutes);
 app.use("/api/customers", customerRoute);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+
+app.get("/", (req, res) => {
+    res.send("✅ Ringnet Invoice API Running");
+  });
+
 
 export default app;
