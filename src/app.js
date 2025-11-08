e3d496a0b71d97f3470fc89c8afd53667b52136e
zainalsalamun <zainalrtf@gmail.com>
2025-11-08 14:21:23 +0700

    update : Inisialisasi struktur backend dengan Express dan PostgreSQL. Menambahkan konfigurasi koneksi database, environment variable (.env), serta setup server.js.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import invoiceRoutes from "./routes/invoiceRoutes.js";
+import customerRoute from "./routes/customerRoute.js"; 
 
 dotenv.config();
 const app = express();
@@ -12,5 +13,6 @@ app.use(express.json());
 
 // routes
 app.use("/api/invoices", invoiceRoutes);
+app.use("/api/customers", customerRoute);
 
 export default app;
