8b88d9802cbe9180a79abe08e6c0892a7a654691
zainalsalamun <zainalrtf@gmail.com>
2025-11-12 13:59:48 +0700

    add : route get invoices

--- src/routes/InvoiceRoutes.js
+++ src/routes/InvoiceRoutes.js
@@ -6,6 +6,7 @@ import {
   editInvoice,
   removeInvoice,
   uploadProof,
+  getInvoiceStats
 } from "../controllers/invoiceController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 import { uploadInvoiceProof } from "../middleware/uploadMiddleware.js";
@@ -17,6 +18,7 @@ router.get("/:id", verifyToken, getInvoice);
 router.post("/", verifyToken, addInvoice);
 router.put("/:id", verifyToken, editInvoice);
 router.delete("/:id", verifyToken, removeInvoice);
+router.get("/stats/monthly", getInvoiceStats);
 
 router.put(
   "/:id/upload",
